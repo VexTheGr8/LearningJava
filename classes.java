cde5c3c33a0d07ed8c65b6741980c5b5e88241da
VexTheGr8 <73146111+VexTheGr8@users.noreply.github.com>
2021-04-05 20:10:16 -0400

    Add files via upload

--- classes.java
+++ classes.java
@@ -1,3 +1,34 @@
 public class classes {
+    public static void main(String[] args) {
+        //classes
+
+        String cpu;
+        String pc_case;
+        boolean DoesWork;
+
+        Computer mypc = new Computer(cpu = "Intel i7",
+         pc_case = "Glass PC Case",
+         DoesWork = true);
+         System.out.println(mypc);
+    }
+
+    static class Computer { //class that declares what variables somethings needs to be a variable
+        
+        String cpu;
+        String pc_case; 
+        boolean DoesWork;
+
+        Computer(String brand, 
+        String pc_case, 
+        boolean DoesWork){
+
+            this.cpu = cpu;
+            this.pc_case = pc_case;
+            this.DoesWork = DoesWork;
+
+
+        }
+
+    }
     
 }
